ca25ee55fd974e57c5fdd912e1335475f3d5ea5a
Vasileios Parousis <parousisv@gmail.com>
2021-05-18 23:04:22 +0300

    Navigation Menu Changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,20 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+//= require rails-ujs
+//= require jquery
+//= require activestorage
+//= require turbolinks
 //= require semantic-ui
+//= require_tree .
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+$(document).on('turbolinks:load', function functionName() {
+  $('.ui.dropdown').dropdown();
+  $('.message .close')
+  .on('click', function() {
+    $(this)
+      .closest('.message')
+      .transition('fade')
+    ;
+  });
+})
