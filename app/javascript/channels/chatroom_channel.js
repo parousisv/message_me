766e819bb82e67815d5ab0db96100383dd2144e6
Vasileios Parousis <parousisv@gmail.com>
2021-06-06 12:13:36 +0300

    Auto-scrolling

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -10,6 +10,7 @@ consumer.subscriptions.create("ChatroomChannel", {
   },
 
   received(data) {
-    $('#message-container').append(data.mod_message)
+    $('#message-container').append(data.mod_message);
+    scroll_bottom();
   }
 });
